bf58669d8ee54bf6b099ccc5dba2e8395539d787
Thiago Marques Siqueira <thiagoms.15@gmail.com>
2021-05-19 23:24:27 +0100

    Added port class, Updated printf and added comments

--- kernel.cpp
+++ kernel.cpp
@@ -4,9 +4,42 @@
 void printf(char* str)
 {
   static uint16_t* VideoMemory = (uint16_t*) 0xb8000;
+  static uint8_t x = 0, y = 0;
+
   for(int i = 0; str[i] != '\0'; ++i)
   {
-    VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    switch(str[i])
+    {
+      case '\n':
+        y++;
+        x = 0;
+        break;
+      default:
+        VideoMemory[(80*y) + x] = (VideoMemory[(80*y) + x] & 0xFF00) | str[i];
+        x++;
+        break;
+
+    }
+
+
+    if(x >= 80)
+    {
+      y++;
+      x = 0;
+    }
+
+    if(y >= 25)
+    {
+      for(y = 0; y < 25; y++)
+      {
+        for(x = 0; x < 80; x++)
+        {
+          VideoMemory[(80*y) + x] = (VideoMemory[(80*y) + x] & 0xFF00) | ' ';
+        }
+      }
+      x = 0;
+      y = 0;
+    }
 
   }
 }
@@ -24,7 +57,8 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(void* multiboot_struct, uint32_t magicnumber)
 {
-  printf("Hello World :-)");
+  printf("Hello World :-)\n");
+  printf("This is my new OS");
 
   GlobalDescriptorTable gdt;
 
--- loader.s
+++ loader.s
@@ -1,5 +1,11 @@
+# GRUB will only load our kernel if it complies with the Multiboot spec.
+
+# Multiboot header must contain 3 fields
+# magic    : containing the magic number 0x1BADB002, to identify the header.
+# flags    : specify features of OS to bootloader
+# checksum : the checksum field when added to the fields ‘magic’ and ‘flags’ must give zero.
 .set MAGIC, 0x1badb002
-.set FLAGS, (1<<0 | 1<<1)
+.set FLAGS, (1<<0 | 1<<1) # aligned on page (4KB) and information on memory
 .set CHECKSUM, - (MAGIC + FLAGS)
 
 .section /multiboot
